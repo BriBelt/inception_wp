da5108df25826a7ace9f3ce46db1b7440f88bda9
Brianna Beltran <bbeltran@debian>
2024-06-13 17:33:36 +0200

    Trying to configure wordpress

--- requirements/mariadb/files/initdb.sh
+++ requirements/mariadb/files/initdb.sh
@@ -2,7 +2,13 @@
 set -e
 
 # Start the MariaDB service
+mysql_install_db >/dev/null 2>&1
+
 service mysql start
+
+service mysql status 
+
+sleep 5
 
 # Initialize the database and create user if not already done
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS WordPress;"
