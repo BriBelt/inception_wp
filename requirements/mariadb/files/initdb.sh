62c6b91a3f1b26693a31c6574b107e46d2edb94b
bbeltran <bbeltran@c1r1s4.42madrid.com>
2024-06-13 19:01:26 +0200

    Modified script for mariadb

--- requirements/mariadb/files/initdb.sh
+++ requirements/mariadb/files/initdb.sh
@@ -11,10 +11,14 @@ service mysql status
 sleep 5
 
 # Initialize the database and create user if not already done
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS WordPress;"
-mysql -u root -e "CREATE USER 'user'@'%' IDENTIFIED BY 'password';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON WordPress.* TO 'user'@'%';"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS \´${WORDPRESS_DB_NAME}\´;"
+mysql -u root -e "CREATE USER \´${WORDPRESS_DB_USER}\´@'localhost' IDENTIFIED BY '${WORDPRESS_DB_USER}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON \´${WORDPRESS_DB_NAME}\´.* TO \´${WORDPRESS_DB_USER}\´@'%'IDENTIFIED BY '${WORDPRESS_DB_USER}';"
+
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${WORDPRESS_USER_PASSWORD}';"
+
 mysql -u root -e "FLUSH PRIVILEGES;"
+
 echo "Database and user created."
 
 # Execute the CMD specified in the Dockerfile
