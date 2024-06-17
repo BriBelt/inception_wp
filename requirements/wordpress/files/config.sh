5a0403df854671615e6d3d6eb3bd80d403e93817
bbeltran <bbeltran@c1r1s4.42madrid.com>
2024-06-17 16:08:55 +0200

    Trying to make mariadb work

--- requirements/wordpress/files/config.sh
+++ requirements/wordpress/files/config.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-wp core download --allow-root
+sudo mariadb
 
-wp config create --allow-root
+CREATE DATABASE ${WORDPRESS_DB_NAME};
 
-wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
+#wp core download --allow-root
 
-wp core install --url=$DOMAIN_NAME --title="WordPress Site" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+#wp config create --allow-root
 
-wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=editor --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
+#wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
+
+#wp core install --url=$DOMAIN_NAME --title="WordPress Site" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+
+#wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=editor --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
