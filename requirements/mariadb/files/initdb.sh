f7506c3eb5a97ef4b2033f2df7fee23645cce96d
bbeltran <bbeltran@c1r1s4.42madrid.com>
2024-06-18 11:41:35 +0200

    changed mariadb script

--- requirements/mariadb/files/initdb.sh
+++ requirements/mariadb/files/initdb.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
+
+# Variable that contains the path to the SQL file that will be used to initialize our db
+MYSQL_INIT_FILE="/createdb.sql"
 set -e
 
-# Start the MariaDB service
+# Suppresses all the messages stdout and stderr to the /dev/null path
 mysql_install_db >/dev/null 2>&1
 
-service mysql start
+# Start the MariaDB service
+if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+	rm -f "$MYSQL_INIT_FILE"
+#service mysql start
 
-service mysql status 
+#service mysql status 
 
-sleep 5
 
 # Initialize the database and create user if not already done
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS \´${WORDPRESS_DB_NAME}\´;"
-mysql -u root -e "CREATE USER \´${WORDPRESS_DB_USER}\´@'localhost' IDENTIFIED BY '${WORDPRESS_DB_USER}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON \´${WORDPRESS_DB_NAME}\´.* TO \´${WORDPRESS_DB_USER}\´@'%'IDENTIFIED BY '${WORDPRESS_DB_USER}';"
+	mysql -u root -e "CREATE DATABASE IF NOT EXISTS \´${WORDPRESS_DB_NAME}\´;"
+	mysql -u root -e "CREATE USER \´${WORDPRESS_DB_USER}\´@'localhost' IDENTIFIED BY '${WORDPRESS_DB_USER}';"
+	mysql -u root -e "GRANT ALL PRIVILEGES ON \´${WORDPRESS_DB_NAME}\´.* TO \´${WORDPRESS_DB_USER}\´@'%'IDENTIFIED BY '${WORDPRESS_DB_USER}';"
 
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${WORDPRESS_USER_PASSWORD}';"
+	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${WORDPRESS_USER_PASSWORD}';"
 
-mysql -u root -e "FLUSH PRIVILEGES;"
-
-echo "Database and user created."
-
+	mysql -u root -e "FLUSH PRIVILEGES;"
+	mysqld_safe --init-file=$MYSQL_INIT_FILE >/dev/null 2>&1
+else
+	mysqld_safe >/dev/null 2>&1
 # Execute the CMD specified in the Dockerfile
 exec "$@"
 
