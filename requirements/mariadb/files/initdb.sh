de6d4c24c42fbce77930c08e316e1d392ccb8eff
bbeltran <bbeltran@c1r1s4.42madrid.com>
2024-06-19 16:44:36 +0200

    Printing messages to see if mariadb is being installed

--- requirements/mariadb/files/initdb.sh
+++ requirements/mariadb/files/initdb.sh
@@ -11,19 +11,14 @@ chmod 777 /var/lib/mysql
 mysql_install_db >/dev/null 2>&1
 
 # Start the MariaDB service
-	echo "CREATE DATABASE $MYSQL_DATABASE;" >> "$MYSQL_INIT_FILE"
-	echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> "$MYSQL_INIT_FILE"
-	echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> "$MYSQL_INIT_FILE"
-	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> "$MYSQL_INIT_FILE"
-	echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> "$MYSQL_INIT_FILE"
-	echo "FLUSH PRIVILEGES;" >> "$MYSQL_INIT_FILE"
-#	mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql >/dev/null 2>&1
-# Initialize the database and create user if not already done
-#	mysql -u root -e "CREATE DATABASE IF NOT EXISTS \´${WORDPRESS_DB_NAME}\´;"
-#	mysql -u root -e "CREATE USER \´${WORDPRESS_DB_USER}\´@'localhost' IDENTIFIED BY '${WORDPRESS_DB_USER}';"
-#	mysql -u root -e "GRANT ALL PRIVILEGES ON \´${WORDPRESS_DB_NAME}\´.* TO \´${WORDPRESS_DB_USER}\´@'%'IDENTIFIED BY '${WORDPRESS_DB_USER}';"
+echo "Creating and configuring database, users, etc..."
+echo "CREATE DATABASE $MYSQL_DATABASE;" >> "$MYSQL_INIT_FILE"
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> "$MYSQL_INIT_FILE"
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> "$MYSQL_INIT_FILE"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> "$MYSQL_INIT_FILE"
+echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> "$MYSQL_INIT_FILE"
+echo "FLUSH PRIVILEGES;" >> "$MYSQL_INIT_FILE"
 
-#	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${WORDPRESS_USER_PASSWORD}';"
+echo "Booting up the database..."
 
-#	mysql -u root -e "FLUSH PRIVILEGES;"
-	mysqld_safe --init-file=$MYSQL_INIT_FILE >/dev/null 2>&1
+mysqld_safe --init-file=$MYSQL_INIT_FILE >/dev/null 2>&1
