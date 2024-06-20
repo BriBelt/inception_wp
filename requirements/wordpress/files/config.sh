f53daa08b3dce92245de85f61bcebad83034e9e2
bbeltran <bbeltran@c1r1s4.42madrid.com>
2024-06-20 17:24:53 +0200

    Now creates the php file

--- requirements/wordpress/files/config.sh
+++ requirements/wordpress/files/config.sh
@@ -13,24 +13,28 @@ echo "$WORDPRESS_DB_HOST"
 #    echo "Waiting for MariaDB..."
 #    sleep 2
 #done
+echo "Downloading WordPress..."
+wp core download --allow-root
 
 if [ -f ./wp-config.php ]; then
 	echo "Wordpress is already installed"
 else
-
-	echo "Downloading WordPress..."
-	wp core download --allow-root
-
-	echo "Creating Wordpress/Mariadb connection..."
-	wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
-
-	echo "Installing WordPress..."
-	wp core install --url=$DOMAIN_NAME --title="WordPress Site" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
-
-	echo "Creating a user..."
-	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=editor --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
-	echo "Wordpress was correctly installed!"
+	mv wp-config.php.custom wp-config.php
 fi
+
+#	echo "Downloading WordPress..."
+#	wp core download --allow-root
+
+#	echo "Creating Wordpress/Mariadb connection..."
+#	wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
+
+#	echo "Installing WordPress..."
+#	wp core install --url=$DOMAIN_NAME --title="WordPress Site" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+
+#	echo "Creating a user..."
+#	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=editor --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
+#	echo "Wordpress was correctly installed!"
+#fi
 
 
 /usr/sbin/php-fpm7.4 --nodaemonize
